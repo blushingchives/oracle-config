ec5f32cf48f5e4b425cc6cb5bdf04071a44c7e50
blushingchives <52131462+blushingchives@users.noreply.github.com>
2023-12-30 22:16:52 +0800

    update

--- generate.sh
+++ generate.sh
@@ -6,5 +6,5 @@ if [ -z $chain_name ]; then
     exit
 fi
 
-cat ${BASH_SOURCE[0]}/$chain_name/* > config.toml
+cat $chain_name/* > config.toml
 echo "SUCCESS: config.toml for $chain_name has been generated."
